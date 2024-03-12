614b5e071c51c6316e7dc9ed81b473260d647d59
Cjawatt <161266575+Cjawatt@users.noreply.github.com>
2024-03-13 09:12:02 +1300

    Add files via upload

---
+++ P_02_variables.py
@@ -0,0 +1,14 @@
+# Code to learn about variables...
+
+# Variables can be a string (note the speach marks)
+greeting = "hello"
+
+# Variables can be a number
+fav_num = 7
+num_string = "5"
+
+# Constants are uppercase
+PI = 3.14
+
+a_01_my_num = 7
+
